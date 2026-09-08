2c5becfa164b87f3e3008a365a564e10514d3980
Chocobe <kyw05171@gmail.com>
2026-09-08 18:11:52 +0900

    [섹션 3] 15. 주문과 할인 도메인 개발

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private MemberRepository memberRepository = new MemoryMemberRepository();
+    private DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = this.memberRepository.findById(memberId);
+        int discountPrice = this.discountPolicy.discount(member, itemPrice);
+
+        return new Order(
+                memberId,
+                itemName,
+                itemPrice,
+                discountPrice
+        );
+    }
+}
