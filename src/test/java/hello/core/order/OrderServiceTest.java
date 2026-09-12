dc96793444e6fe0a79e6eca83ecdd015bc085777
Chocobe <kyw05171@gmail.com>
2026-09-12 22:24:37 +0900

    [섹션 8] 45. (깨진 테스트 케이스 복원)

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -28,7 +28,7 @@ public class OrderServiceTest {
         memberService.join(member);
 
         Order order = orderService.createOrder(memberId, "itemA", 20000);
-        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+        assertThat(order.getDiscountPrice()).isEqualTo(2000);
     }
 
 }
